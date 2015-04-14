40769b813371212ff3d0a3aa43742c14ab0e394e
Andrew Thal <athal7@me.com>
2015-04-14 15:25:12 -0400

    Fix specs

--- spec/support/authentication_support.rb
+++ spec/support/authentication_support.rb
@@ -1,8 +1,7 @@
 module AuthenticationSupport
   def stub_api_controller_authentication!(options={})
     @current_api_user = options[:admin] ? create(:admin_user) : create(:user)
-    warden = double(:warden, user: @current_api_user, authenticate: @current_api_user)
-    controller.stub(:env).and_return({'warden' => warden})
-    controller.request.env['warden'] = warden
+    controller.stub(:load_user)
+    controller.instance_variable_set(:@current_api_user, @current_api_user)
   end
 end
