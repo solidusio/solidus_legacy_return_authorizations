424bc13c7feb28110f462d4b1e7d11f76885a9b9
Jordan Brough <git.j@brgh.net>
2015-12-16 23:58:23 -0700

    Fix method override to include block
    Compatibility with upcoming navigation changes.

--- app/helpers/spree/admin/navigation_helper_decorator.rb
+++ app/helpers/spree/admin/navigation_helper_decorator.rb
@@ -1,11 +1,16 @@
+# This makes it so that the Orders tab is highlighted when we're in the legacy
+# return authorization area.
+
+# TODO: Append this to Spree::BackendConfiguration::ORDER_TABS instead of monkey
+# patching this method.
 Spree::Admin::NavigationHelper.module_eval do
-  def tab_with_legacy_return_authorizations(*args)
+  def tab_with_legacy_return_authorizations(*args, &block)
     if args.first == :orders
       options = args.pop if args.last.is_a?(Hash)
       args << :legacy_return_authorizations
       args << options
     end
-    tab_without_legacy_return_authorizations(*args)
+    tab_without_legacy_return_authorizations(*args, &block)
   end
 
   alias_method_chain :tab, :legacy_return_authorizations
